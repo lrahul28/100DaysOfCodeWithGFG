27f005f1d1df93a8f5dc016ea71492124c449a42
Rahul <lrahul28@gmail.com>
2021-09-16 19:49:44 +0530

    Day2

---
+++ Day2.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findMaximumRooms(vector<int> &start, vector<int> &end, int n)
+{
+	vector<pair<int, int>> p;
+	for(int i = 0; i < n; i++)
+	{
+		p.push_back({end[i], start[i]});
+	}
+	int c = 1;
+	sort(p.begin(), p.end());
+	int currend = p[0].first;
+	for(int i = 1; i < n; i++)
+	{
+		if(p[i].second > currend) 
+		{
+		  c++;
+		  currend = p[i].first;
+		}
+
+	}
+	return c;
+} 
+int main()
+{
+#ifndef IO_HANDLE
+	freopen("C:/Users/Rahul/AppData/Roaming/Sublime Text 3/Packages/User/input.txt", "r", stdin);
+	freopen("C:/Users/Rahul/AppData/Roaming/Sublime Text 3/Packages/User/output.txt", "w", stdout);
+#endif	
+	int n;
+	cin >> n;
+	vector<int> start(n);
+	vector<int> end(n);
+	for(int i = 0; i < n; i++) cin >> start[i];
+	for(int i = 0; i < n; i++) cin >> end[i];
+	cout << findMaximumRooms(start, end, n);
+}
