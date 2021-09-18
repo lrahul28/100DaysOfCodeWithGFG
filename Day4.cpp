78c55752fd258f9d2f286cd7500426661a6a6c7c
Rahul <lrahul28@gmail.com>
2021-09-18 18:42:08 +0530

    Day4

---
+++ Day4.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> sumQuery2(vector<int> &arr, vector<int> &queries)
+{
+		unordered_map<int, int> mp;
+        int sum = 0;
+        int n = arr.size();
+        for(int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+            mp[i] = sum;
+        }
+        vector<int> res;
+        for(int i = 0; i < queries.size(); i += 2)
+        {
+            res.push_back(mp[queries[i + 1] - 1] - mp[queries[i] - 2]);
+        }
+        return res;
+}
+int main()
+{
+#ifndef IO_HANDLE
+	freopen("C:/Users/Rahul/OneDrive/Desktop/Sublime Text 3/Packages/User/input.txt", "r", stdin);
+	freopen("C:/Users/Rahul/OneDrive/Desktop/Sublime Text 3/Packages/User/output.txt", "w", stdout);
+#endif	
+
+	
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for(int i = 0; i < n; i++) cin >> v[i];
+	int q;
+	cin >> q;
+	vector<int> query(2 * q);
+	for(int i = 0; i < 2 * q; i++) cin >> query[i];
+	vector<int> res = sumQuery2(v, query);
+	for(int x : res) cout << x << "\n";
+
+
+}
