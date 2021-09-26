a105f742172ee003ee5750aef87f5f27325c3fe4
Rahul <lrahul28@gmail.com>
2021-09-26 21:08:44 +0530

    Day12

---
+++ Day12.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+int search(string s, string pat)
+{
+	map<char, int> mp;
+	
+	for(int i = 0; i < pat.length(); i++) mp[pat[i]]++;
+
+	int c = mp.size();
+	
+	int i = 0, j = 0;
+	
+	int ans = 0;
+	while(j < s.length())
+	{
+		
+		if(j - i < pat.length())
+		{
+			if(mp[s[j]] == 1)
+			{
+			 c--;
+			 if(c == 0) ans++;
+			}
+			mp[s[j]]--;
+			j++;
+		}
+		else
+		{
+
+			if(mp[s[i]] == 0) 
+			{
+				c++;
+				if(c == 0) ans++;
+			}
+			mp[s[i]]++;
+			i++;
+			
+		}
+	}
+	return ans;
+
+}
+
+int main()
+{
+#ifndef IO_HANDLE
+	freopen("C:/Users/Rahul/OneDrive/Desktop/Sublime Text 3/Packages/User/input.txt", "r", stdin);
+	freopen("C:/Users/Rahul/OneDrive/Desktop/Sublime Text 3/Packages/User/output.txt", "w", stdout);
+#endif	
+	string txt, pat;
+	cin >> txt;
+	cin >> pat;
+	cout << search(txt, pat);
+}
